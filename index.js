eb9760dc9664b9ac64d25b2d31321ed310fe3b24
maciejSzcz <yetek.lol@gmail.com>
2020-02-29 18:02:59 +0100

    created authentication and connected to mongodb

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-
-const PORT = process.env.port || 3000
-
+const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
+const User = require('./model/User');
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(`${process.env.DB_CONNECT}`, 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }, () => console.log('DB Connected!')
+);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 
 // Middlewares
+app.use(express.json());
 
 app.use('/api/user', authRoute)
 
-app.listen(3000, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,44 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const User = require('../model/User');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string()
+        .min(6)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(6)
+        .required()
 })
+
+
+
+router.post('/register', async (req, res) => {
+    //valiation of data
+    const {error} = schema.validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch(err) {
+        res.status(400).send(err)
+    }
+
+});
 
 
 
